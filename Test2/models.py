af245b893a0761f95a2e1296ba0c60fc43a5ac8f
eRiKa4530 <omurbekovaerika@gmail.com>
2023-02-02 15:28:26 +0600

    django2

--- Test2/models.py
+++ Test2/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from Test1.models import Post
 
 class Commint(models.Model):
     commit= models.CharField(max_length=50,unique=True)
-   
+    category = models.ForeignKey(Post,on_delete= models.CASCADE,related_name='commints')
 
     
     def __str__(self):
